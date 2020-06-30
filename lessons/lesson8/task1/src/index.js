802cc95689ca31db91be63de7e53a786ec3391d3
ptol86 <ah021714@gmail.com>
2020-06-30 22:54:08 +0300

    test

---
+++ lessons/lesson8/task1/src/index.js
@@ -0,0 +1,9 @@
+import renderProfile from './profile/renderProfile';
+import './index.css';
+
+const profileData = {
+    name: 'Leha',
+    location: 'The Dnipro'
+};
+
+renderProfile(profileData);
